6a85ea210916df2ceae2cc1aeb8056da17355b42
Wellington Gomes <wgmachado@inf.ufpel.edu.br>
2023-01-03 23:25:26 -0300

    04_DML_2

--- 04_DML_2.sql
+++ 04_DML_2.sql
@@ -180,3 +180,81 @@ where cidade = 'PORTO ALEGRE';
 
 delete from aluno
 where matricula = 3;
+
+--EXERCICIOS
+-- 1) SELECIONE TODOS OS ALUNOS QUE NÃO POSSUEM TELEFONE E COLOQUE EM ALGUNS A INFORMAÇÃO DO TELEFONE
+
+update aluno
+set telefone = 999880741
+where telefone is null;
+
+-- 2) ATUALIZE OS ALUNOS, AUMENTANDO EM UM ANO A IDADE DE TODOS OS ALUNOS QUE NASCERAM DEPOIS DE 01/01/1980
+
+update aluno
+set idade = idade + 1
+where dtNascimento > '01-01-1992';
+
+-- 3) ATUALIZE AS DISCIPLINAS, DEIXANDO TODAS AS CARGAS HORARIA IGUAL A 60
+
+update disciplina
+set cargaHoraria = 60;
+
+-- 4) CRIE UMA CONSULTA PARA EXIBIR O NOME E O SALARIO DOS FUNCIONARIOS QUE RECEBEM MAIS DE 800 REAIS
+
+select nome, salario
+from funcionario
+where salario > 800;
+
+-- 5) CRIE UMA CONSULTA PARA EXIBIR O NOME DO FUNCIONARIO E O NUMERO DO DEPARTAMENTO PARA O CÓDIGO
+-- DO FUNCIONARIO 
+
+update funcionario
+set codDepartamento = 459
+where nome in ('PELÉ', 'MARADONA');
+
+select nome, codDepartamento
+from funcionario
+where codDepartamento = 459;
+
+-- 6) EXIBA O NOME E O SALARIO DE TODOS OS FUNCIONARIOS CUJOS SALARIOS NAO ESTEJAM NA FAIXA ENTRE 
+-- 950 E 2300 REAIS
+
+select nome, salario
+from funcionario
+where salario not between 950 and 2300;
+
+-- 7) EXIBA O NOME DO FUNCIONARIO, O CARGO E A DATA DOS FUNCIONARIOS ADMITIDOS ENTRE 20 DE FEVEREIRO
+-- DE 1988 E 1 DE MAIO DE 1992
+
+select nome, cargo, dtContratacao
+from funcionario
+where dtContratacao between '1988-02-20' and '1992-05-01';
+
+-- 8) EXIBA O NOME DO FUNCIONARIO E O NUMERO DO DEPARTAMENTO DE TODOS OS FUNCIONARIOS DOS DEPARTAMENTOS 10 E 30
+-- POR ORDEM ALFABETICA DE NOME
+
+update funcionario
+set codDepartamento = 10
+where cod in ('1290','1291','1292');
+
+update funcionario
+set codDepartamento = 30
+where cod in ('1293', '1294', '1295');
+
+select nome, codDepartamento
+from funcionario
+where codDepartamento in ('10', '30')
+order by nome asc;
+
+-- 9) LISTE O NOME E O SALARIO DOS FUNCIONARIOS QUE RECEBEM MAIS DE 1500 E QUE ESTAO NOS DEPARTAMENTOS
+-- 10 OU 30. NOMEIE AS COLUNAS NOME E SALARIO, PARA FUNCIONARIO E SALARIO DO MES
+
+select nome as funcionario, salario as salario_do_mes, codDepartamento
+from funcionario
+where salario > 1500 and codDepartamento in ('10', '30');
+
+-- 10) EXIBA O NOME E A DATA DE ADMISSAO DE CADA FUNCIONARIO ADMITIDO EM 1995-01-01
+
+select nome, dtContratacao 
+from funcionario
+where dtContratacao >= '1995-01-01';
