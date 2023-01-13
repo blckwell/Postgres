ab9737fb4ccd6dc7fd6a24221cf39c436abe9e2e
Wellington Gomes <wgmachado@inf.ufpel.edu.br>
2023-01-12 21:33:11 -0300

    05_DML_3

--- 05_DML_3.sql
+++ 05_DML_3.sql
@@ -92,3 +92,41 @@ where telefone is not null
 group by lower(cidade)
 having max(idade) >= 18
 order by 2 desc;
+
+-- EXERCICIOS
+-- 1) CRIE UMA CONSULTA PARA EXIBIR DATA ATUAL
+
+select current_date;
+
+-- 2) CRIE UMA CONSULTA PARA INFORMAR O DIA DA SEMANA ATUAL (SEGUNDA, TERCA, QUARTA)
+
+select extract (dow from current_date) as semana_numerica,
+case when extract(dow from current_date) = 0 then 'Domingo'
+	 when extract(dow from current_date) = 1 then 'Segunda-feira'
+	 when extract(dow from current_date) = 2 then 'Terca-feira'
+	 when extract(dow from current_date) = 3 then 'Quarta-feira'
+	 when extract(dow from current_date) = 4 then 'Quinta-feira'
+	 when extract(dow from current_date) = 5 then 'Sexta-feira'
+	 when extract (dow from current_date) = 6 then 'Sabado'
+	 else 'other'
+end;
+
+-- 3) EXIBA O CODIGO DO FUNCIONARIO, O NOME, O SALARIO E O AUMENTO SALARIAL DE 15 POR CENTO 
+-- EXPRESSO COMO NUMERO INTEIRO 
+
+select nome, salario, salario + ((salario*15)/100) as aumento_salarial 
+from funcionario;
+
+-- 4) ADICIONE A CONSULTA ANTERIOR UMA COLUNA QUE CONTERÁ O SALÁRIO NOVO MENOS O SALÁRIO ANTIGO.
+
+select salario, salario + ((salario*15/100)) as salario_novo, salario - ((salario*15/100)) as salario_antigo
+from funcionario;
+
+-- 5) EXIBA O NOME DO FUNCIONARIO, A DATA DE ADMISSÃO E A DATA DE EFETIVACAO, QUE É SEIS MESES APOS A ADMISSÃO.
+--	  (tabela não possui colunas exigidas)
+
+-- 6) Para cada funcionário exiba o nome do mesmo e calcule o número de meses entre a hoje e a sua data de
+-- admissão. Ordene a consulta pelo número de meses. Arredonde para inteiro o número de meses.
+
+select nome, extract(month from dtContratacao) as numero_de_meses
+from funcionario;
